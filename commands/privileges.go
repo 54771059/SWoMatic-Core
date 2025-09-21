4fb40c5f623ba315b985751465c2478d0dd9c132
54771059 <darkvalvet@protonmail.com>
2025-09-21 11:24:40 +0300

    fix & feat: fixed bug with asking entry always, added function to elevate privilege

--- commands/privileges.go
+++ commands/privileges.go
@@ -1,6 +1,7 @@
 package privs
 
 import (
+	"SWoMatic-Core/internal/io"
 	"bufio"
 	"fmt"
 	"os"
@@ -33,15 +34,15 @@ func detectPasswordPrompt(output string) bool {
 	return false
 }
 
-func PasswordHandler(port serial.Port, output string) {
+func PasswordHandler(port serial.Port, output string, privInvoked bool) {
 	// Handle password prompts and responses
 	if detectPasswordPrompt(output) {
 		// Respond to password prompt
-		if EntryPassword != "" && !usedEntry {
+		if EntryPassword != "" && !usedEntry && !privInvoked {
 			// Use saved entry password
 			port.Write([]byte(EntryPassword + "\n"))
 			usedEntry = true
-		} else {
+		} else if !usedEntry && !privInvoked {
 			// Prompt for entry password
 			fmt.Print("Enter entry password: ")
 			reader := bufio.NewReader(os.Stdin)
@@ -70,5 +71,25 @@ func PasswordHandler(port serial.Port, output string) {
 			PrivilegePassword = strings.TrimSpace(pass)
 			port.Write([]byte(PrivilegePassword + "\n"))
 		}
+	}
+}
+
+func ElevatePrivilege(port serial.Port, vendor string) {
+	port.Write([]byte("\x03"))
+	response := io.ReadOutput(port)
+	if strings.Contains(response, "#") {
+		return
+	}
+	switch strings.ToLower(vendor) {
+	case "cisco":
+		port.Write([]byte("enable\n"))
+		response := io.ReadOutput(port)
+		PasswordHandler(port, response, true)
+	case "aruba":
+		port.Write([]byte("enable\n"))
+		response := io.ReadOutput(port)
+		PasswordHandler(port, response, true)
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown vendor: %s\n", vendor)
 	}
 }
